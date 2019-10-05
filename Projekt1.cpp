4550e70e39de04dfeb2dcb1e392d322faebf9a32
Mykyta Kotenko <56121705+MykytaKotenko@users.noreply.github.com>
2019-10-05 16:56:34 +0300

    Create Projekt1.cpp

---
+++ Projekt1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <conio.h>
+int main()
+{
+
+	std::cout << "Jak sie masz" << std::end1;
+	std::cout << std::end1;
+	std::cout << "Jestes tajnym agentem, ktory wlamuje sie do strzezonej serverowni";
+	std::cout << std::end1;
+
+	//zmienne
+	//stosowne a do przechowywania
+
+	int a = 23;
+	std::cout << a << std::end1;
+	int b = 15;
+	std::cout << b << std::end1;
+
+	int sum = a + b;
+	int sum = a - b;
+	float del = float(a) / float(b);
+	int mnoz = a * b;
+
+	std::cout << "dodawnie: " << sum << std::end1;
+	std::cout << "odejmowanie: " << min << std::end1;
+	std::cout << "mnozenie: " << mnoz << std::end1;
+	std::cout << "podzial: " << del << std::end1;
+
+	std::cout << "Zeby skonce=zyc program nacisnij doolnyj przyisk:)";
+	_getch();
+
+	return 0;
+
+};
